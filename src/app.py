ae34edc065c12551372bc9bbd33151c4d8b28c78
Stig Ofstad <stigofstad@gmail.com>
2022-10-14 14:52:49 +0200

    refactor: move src to project root

---
+++ src/app.py
@@ -0,0 +1,61 @@
+import uvicorn
+from fastapi import APIRouter, FastAPI, Security
+from fastapi.exceptions import RequestValidationError
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from starlette.middleware import Middleware
+from starlette.responses import RedirectResponse
+
+from config import config
+from features.jobs import jobs
+from middleware.store_headers import StoreHeadersMiddleware
+from restful.responses import responses
+from utils.exception_handlers import validation_exception_handler
+
+oauth2_scheme = OAuth2AuthorizationCodeBearer(authorizationUrl="", tokenUrl="")
+
+
+def auth_with_jwt(jwt_token: str = Security(oauth2_scheme)):
+    # Authentication is handled by DMSS. Adding a security dependenciy is
+    # necessary to generate correct JSON schema for openapi generator.
+    pass
+
+
+def create_app():
+    all_routes = APIRouter(tags=["DMJobs"])
+    authenticated_routes = APIRouter()
+    authenticated_routes.include_router(jobs.router)
+
+    app = FastAPI(
+        title="Data Modelling Job API",
+        responses=responses,
+        description="REST API used with the Data Modelling framework to schedule jobs",
+        exception_handlers={RequestValidationError: validation_exception_handler},
+        middleware=[Middleware(StoreHeadersMiddleware)],
+    )
+
+    all_routes.include_router(authenticated_routes, dependencies=[Security(auth_with_jwt)])
+    app.include_router(all_routes)
+
+    @app.get("/", operation_id="redirect_to_docs", response_class=RedirectResponse)
+    def redirect_to_docs():
+        """
+        Redirects any requests to the servers root ('/') to '/docs'
+        """
+        return RedirectResponse(url="/docs")
+
+    return app
+
+
+def run():
+    uvicorn.run(
+        "app:create_app",
+        host="0.0.0.0",  # nosec
+        port=5000,
+        reload=config.ENVIRONMENT == "local",
+        factory=True,
+        log_level=config.LOGGER_LEVEL.lower(),
+    )
+
+
+if __name__ == "__main__":
+    run()
---
+++ src/init.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -eu
+ENVIRON=${ENVIRONMENT:="production"}
+API_ENV=${API_ENV:="production"}
+
+# Wait until the storage services is ready before continuing.
+# This is to ensure that the services is initialized before the API tries to connect.
+service_is_ready() {
+  ATTEMPT_COUNTER=1
+  MAX_ATTEMPTS=100
+  echo "Testing availability of DMSS: $DMSS_API"
+  until $(curl --silent --output /dev/null --fail "$DMSS_API/api/v1/healthcheck"); do
+    if [ ${ATTEMPT_COUNTER} -eq ${MAX_ATTEMPTS} ];then
+      echo "ERROR: Max attempts reached. Data Modelling Storage API($DMSS_API) did not respond. Exiting..."
+      exit 1
+    fi
+
+    echo "Waiting for $DMSS_API... (${ATTEMPT_COUNTER})"
+    ATTEMPT_COUNTER=$((ATTEMPT_COUNTER+1))
+    sleep 5
+  done
+  echo "DMSS is ready!"
+}
+
+install_dmss_package() {
+  if [ ! -e /dmss_api/setup.py ]; then
+    echo "WARNING: Tried to install local version of the DMSS-API, but it could not be found. Continuing with version from Pypi..."
+  else
+    echo "Installing DMSS-API from local..."
+    pip uninstall dmss-api -y -q
+    pip install -e /dmss_api/ -q
+  fi
+}
+
+# Try to install the local version of the DMSS-API pypi package. Soft fail.
+if [ "$ENVIRON" = 'local' ] && [ "$API_ENV" = 'development' ]; then
+  install_dmss_package
+fi
+
+if [ "$1" = 'api' ]; then
+  service_is_ready
+    if [ "$ENVIRON" != "local" ]; then
+    cat version.txt || true
+    gunicorn app:create_app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
+  else
+    python3 ./app.py
+  fi
+else
+  exec "$@"
+fi
