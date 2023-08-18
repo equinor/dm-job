8bfa1601173a2119b0182628be373d2d15bd9a5e
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-08-18 15:56:40 +0200

    refactor rename split_absolute_ref() and add more tests

--- src/tests/unit_tests/test_string_helpers.py
+++ src/tests/unit_tests/test_string_helpers.py
@@ -1,20 +1,49 @@
-from utils.string_helpers import split_absolute_ref
+import unittest
+
+from utils.string_helpers import split_address
 
 
-def test_split_absolute_ref():
-    data_source = "AnalysisPlatformDS"
-    path = "4483c9b0-d505-46c9-a157-94c79f4d7a6a"
-    attribute = "jobs.0"
-    example_reference = f"{data_source}/{path}.{attribute}"
-    data_source_result, path_result, attribute_result = split_absolute_ref(example_reference)
-    assert data_source == data_source_result
-    assert path == path_result
-    assert attribute == attribute_result
+class TestStringHelpers(unittest.TestCase):
+    def test_split_address_1(self):
+        data_source = "DataSource"
+        id = "$4483c9b0-d505"
+        attribute = "jobs.result"
+        example_address = f"{data_source}/{id}.{attribute}"
+        protocol, data_source_result, id_result, attribute_result = split_address(example_address)
+        assert protocol is None
+        assert data_source == data_source_result
+        assert id == id_result
+        assert attribute == attribute_result
 
-    data_source = "AnalysisPlatformDS"
-    path = "4483c9b0-d505-46c9-a157-94c79f4d7a6a"
-    example_reference_2 = f"{data_source}/{path}"
-    data_source_result, path_result, attribute_result = split_absolute_ref(example_reference_2)
-    assert data_source == data_source_result
-    assert path == path_result
-    assert attribute_result == ""
+    def test_split_address_2(self):
+        data_source = "DataSource"
+        path = "root/sub/entity"
+        attribute = "jobs.0"
+        example_address = f"{data_source}/{path}.{attribute}"
+        protocol, data_source_result, path_result, attribute_result = split_address(example_address)
+        assert protocol is None
+        assert data_source == data_source_result
+        assert path == path_result
+        assert attribute == attribute_result
+
+    def test_split_address_3(self):
+        data_source = "DataSource"
+        path = "root_package/sub_package"
+        example_address = f"{data_source}/{path}"
+        protocol, data_source_result, path_result, attribute_result = split_address(example_address)
+        assert protocol is None
+        assert data_source == data_source_result
+        assert path == path_result
+        assert attribute_result == ""
+
+    def test_split_address_4(self):
+        protocol = "dmss"
+        data_source = "DataSource"
+        id = "$4483c9b0-d505-x"
+        attribute = "jobs.0.content[0]"
+        example_address = f"{protocol}://{data_source}/{id}.{attribute}"
+        protocol_result, data_source_result, id_result, attribute_result = split_address(example_address)
+        assert protocol == protocol_result
+        assert data_source == data_source_result
+        assert id == id_result
+        assert attribute == attribute_result
